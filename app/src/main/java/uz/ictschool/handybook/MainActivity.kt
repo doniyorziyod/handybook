53d5d040978bb4044a80dfe26e8faab4a84f67b9
ziyodovdoniyor <ziyodovdoniyorcode@gmail.com>
2023-10-23 13:13:37 +0500

    first commit

---
+++ app/src/main/java/uz/ictschool/handybook/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.ictschool.handybook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
