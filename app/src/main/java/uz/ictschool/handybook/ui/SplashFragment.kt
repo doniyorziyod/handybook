0c8a764557b4a9558cb92ce85cda4afbf6d14f6d
User_4 <elshodxushvaqtov07@gmail.com>
2023-10-27 08:38:10 +0500

    Shamsiddin

--- app/src/main/java/uz/ictschool/handybook/ui/SplashFragment.kt
+++ app/src/main/java/uz/ictschool/handybook/ui/SplashFragment.kt
@@ -11,19 +11,10 @@ import androidx.fragment.app.Fragment
 import okhttp3.internal.wait
 import uz.ictschool.handybook.R
 import uz.ictschool.handybook.databinding.FragmentSplashBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SplashFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -42,8 +33,8 @@ class SplashFragment : Fragment() {
         val binding = FragmentSplashBinding.inflate(inflater, container, false)
 
         var shake = AnimationUtils.loadAnimation(requireContext(), R.anim.shake)
-        var replenish = AnimationUtils.loadAnimation(requireContext(), R.anim.replenish)
-        var dissapear = AnimationUtils.loadAnimation(requireContext(), R.anim.dissappear)
+        val replenish = AnimationUtils.loadAnimation(requireContext(), R.anim.replenish)
+        val dissapear = AnimationUtils.loadAnimation(requireContext(), R.anim.dissappear)
 
 
 //            binding.imageView.startAnimation(dissapear)
@@ -71,7 +62,7 @@ class SplashFragment : Fragment() {
             })
 
         binding.login.setOnClickListener {
-            parentFragmentManager.beginTransaction().replace(R.id.main,SignInFragment()).commit()
+            parentFragmentManager.beginTransaction().replace(R.id.main,HomeFragment()).commit()
         }
         binding.register.setOnClickListener {
             parentFragmentManager.beginTransaction().replace(R.id.main,SignUpFragment()).commit()
@@ -81,15 +72,6 @@ class SplashFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SplashFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             SplashFragment().apply {
