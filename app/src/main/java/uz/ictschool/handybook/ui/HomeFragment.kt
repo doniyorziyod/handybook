77682f2f97b807cf9d3d2435e133bb9f6854e7de
qa598 <farrukhburiev001@gmail.com>
2023-11-02 20:51:49 +0500

    home layout

--- app/src/main/java/uz/ictschool/handybook/ui/HomeFragment.kt
+++ app/src/main/java/uz/ictschool/handybook/ui/HomeFragment.kt
@@ -5,12 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import farrukh.remotely.adapter.CategoryAdapter
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import uz.ictschool.handybook.R
+import uz.ictschool.handybook.adapter.BookAdapter
 import uz.ictschool.handybook.api.APIClient
 import uz.ictschool.handybook.api.APIService
 import uz.ictschool.handybook.data.Book
@@ -33,6 +36,7 @@ class HomeFragment : Fragment() {
         }
     }
 
+    lateinit var currentcategory: String
     lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,64 +46,169 @@ class HomeFragment : Fragment() {
 //        binding.imageView2.setOnClickListener {
 //            parentFragmentManager.beginTransaction().replace(R.id.main, ProfileFragment()).commit()
 //        }
-        val api = APIClient.getInstance().create(APIService::class.java)
-        var categories = mutableListOf<CategoryData>()
-        var books = mutableListOf<Book>()
+//        val api = APIClient.getInstance().create(APIService::class.java)
+//        val categories = mutableListOf<CategoryData>()
+//        currentcategory = ""
+//
+//
+//        //Category Recycler
+//        api.getAllCategory().enqueue(object : Callback<List<CategoryData>> {
+//            override fun onResponse(
+//                call: Call<List<CategoryData>>,
+//                response: Response<List<CategoryData>>
+//            ) {
+//                for (i in 0 until response.body()!!.size) {
+//                    categories.add(response.body()!![i])
+//                }
+//                if (categories.isNotEmpty()) {
+//                    val adapter =
+//                        CategoryAdapter(
+//                            categories,
+//                            requireContext(),
+//                            object : CategoryAdapter.ItemClick {
+//                                override fun OnItemClick(category: String) {
+//                                    currentcategory = category
+//                                    if (!response.body()!!.contains(CategoryData(category))){
+//                                        api.getAllBooks().enqueue(object : Callback<List<Book>>{
+//                                            override fun onResponse(
+//                                                call: Call<List<Book>>,
+//                                                response: Response<List<Book>>
+//                                            ) {
+//                                                binding.homeKitoblarRecycler.visibility = View.VISIBLE
+//                                                binding.homeNotfound.visibility = View.GONE
+//                                                binding.homeKitoblarRecycler.adapter = BookAdapter(response.body()!!, object : BookAdapter.OnPressed{
+//                                                    override fun onPressed(book: Book) {
+//                                                        parentFragmentManager.beginTransaction().replace(R.id.main, BookViewFragment()).addToBackStack("Home").commit()
+//                                                    }
+//                                                })
+//                                                Log.d(TAG, "onResponse: $category, ${response.body()},")
+//                                            }
+//
+//                                            override fun onFailure(
+//                                                call: Call<List<Book>>,
+//                                                t: Throwable
+//                                            ) {
+//                                                Log.d(TAG, "onFailure: $t")
+//                                            }
+//
+//                                        })
+//                                    }else{
+//                                        api.getBooksByCategory(category).enqueue(object : Callback<List<Book>>{
+//                                            override fun onResponse(
+//                                                call: Call<List<Book>>,
+//                                                response: Response<List<Book>>
+//                                            ) {
+//                                                if (response.body()?.isNotEmpty()!!){
+//                                                    binding.homeKitoblarRecycler.visibility = View.VISIBLE
+//                                                    binding.homeNotfound.visibility = View.GONE
+//                                                    binding.homeKitoblarRecycler.adapter = BookAdapter(response.body()!!, object : BookAdapter.OnPressed{
+//                                                        override fun onPressed(book: Book) {
+//                                                            parentFragmentManager.beginTransaction().replace(R.id.main, BookViewFragment()).addToBackStack("Home").commit()
+//                                                        }
+//                                                    })
+//                                                }else{
+//                                                    binding.homeKitoblarRecycler.visibility = View.GONE
+//                                                    binding.homeNotfound.visibility = View.VISIBLE
+//                                                }
+//                                                Log.d(TAG, "onResponse: $category,  ${response.body()}")
+//                                            }
+//
+//                                            override fun onFailure(
+//                                                call: Call<List<Book>>,
+//                                                t: Throwable
+//                                            ) {
+//                                                Log.d(TAG, "onFailure: $t")
+//                                            }
+//
+//                                        })
+//                                    }
+//
+//                                }
+//                            })
+//
+//                    val manager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+//                    binding.category.layoutManager = manager
+//                    binding.category.adapter = adapter
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<CategoryData>>, t: Throwable) {
+//                Log.d(TAG, "onFailure: $t")
+//            }
+//        })
+//
+//        //Books Recycler
+//        api.getAllBooks().enqueue(object : Callback<List<Book>>{
+//            override fun onResponse(
+//                call: Call<List<Book>>,
+//                response: Response<List<Book>>
+//            ) {
+//                binding.homeKitoblarRecycler.adapter = BookAdapter(response.body()!!, object : BookAdapter.OnPressed{
+//                    override fun onPressed(book: Book) {
+//                        parentFragmentManager.beginTransaction().replace(R.id.main, BookViewFragment()).addToBackStack("Home").commit()
+//                    }
+//                })
+//            }
+//
+//            override fun onFailure(
+//                call: Call<List<Book>>,
+//                t: Throwable
+//            ) {
+//                Log.d(TAG, "onFailure: $t")
+//            }
+//        })
+//
+//
+//        //Search Stuff
+//        binding.search.setOnQueryTextListener(object : OnQueryTextListener{
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                return true
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                if (newText!=null){
+//                    api.search(newText).enqueue(object : Callback<List<Book>>{
+//                        override fun onResponse(
+//                            call: Call<List<Book>>,
+//                            response: Response<List<Book>>
+//                        ) {
+//                            if (response.body()?.isNotEmpty()!!){
+//                                binding.homeKitoblarRecycler.adapter = BookAdapter(response.body()!!, object : BookAdapter.OnPressed{
+//                                    override fun onPressed(book: Book) {
+//                                        parentFragmentManager.beginTransaction().replace(R.id.main, BookViewFragment()).addToBackStack("Home").commit()
+//                                    }
+//                                })
+//                                binding.homeKitoblarRecycler.visibility = View.VISIBLE
+//                                binding.homeNotfound.visibility = View.GONE
+//                            }else{
+//                                binding.homeKitoblarRecycler.visibility = View.GONE
+//                                binding.homeNotfound.visibility = View.VISIBLE
+//                            }
+//                        }
+//
+//                        override fun onFailure(call: Call<List<Book>>, t: Throwable) {
+//                            Log.d("SearchTAG", "onFailure: $t")
+//                        }
+//
+//                    })
+//                    return true
+//                }
+//                binding.homeKitoblarRecycler.visibility = View.VISIBLE
+//                binding.homeNotfound.visibility = View.GONE
+//                return false
+//            }
+//
+//        })
 
-        api.getAllCategory().enqueue(object : Callback<List<CategoryData>> {
-            override fun onResponse(
-                call: Call<List<CategoryData>>,
-                response: Response<List<CategoryData>>
-            ) {
-                for (i in 0 until response.body()!!.size) {
-                    categories.add(response.body()!!.get(i))
-                }
-                if (categories.isNotEmpty()) {
-                    var adapter =
-                        CategoryAdapter(
-                            categories,
-                            requireContext(),
-                            object : CategoryAdapter.ItemClick {
-                                override fun OnItemClick(category: String) {
-                                    books.clear()
-
-                                    api.getBookByCategory(category).enqueue(object :Callback<List<Book>>{
-                                        override fun onResponse(
-                                            call: Call<List<Book>>,
-                                            response: Response<List<Book>>
-                                        ) {
-                                            Log.d(TAG, "onResponse: ${response.body()}")
-                                        }
-
-                                        override fun onFailure(
-                                            call: Call<List<Book>>,
-                                            t: Throwable
-                                        ) {
-                                            Log.d(TAG, "onResponse: $t")
-                                        }
-
-                                    })
-
-                                }
-
-                            })
-
-                    var manager =
-                        LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-                    binding.category.layoutManager = manager
-                    binding.category.adapter = adapter
-                }
 
 
-            }
 
-            override fun onFailure(call: Call<List<CategoryData>>, t: Throwable) {
-                Log.d(TAG, "onFailure: $t")
-            }
 
-        })
+
+
         return binding.root
     }
+
 
     companion object {
         @JvmStatic
@@ -111,4 +220,5 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
 }
